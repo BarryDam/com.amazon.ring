1c9cebdeeac7428272fb483c1106097493798c9b
BarryDam <barrydam@MacBook-Pro-van-Barry.local>
2025-10-01 18:41:15 +0200

    working

--- app.js
+++ app.js
@@ -6,82 +6,95 @@ const api = require('./lib/Api.js');
 const events = require('events');
 
 // !!!! remove next lines before publishing !!!!
- const LogToFile = require('homey-log-to-file'); // https://github.com/robertklep/homey-log-to-file
+const LogToFile = require('homey-log-to-file'); // https://github.com/robertklep/homey-log-to-file
 
 let capturedStderr = '';
 const originalStderrWrite = process.stderr.write;
 // process.env.DEBUG = '*'
 
 class App extends Homey.App {
-
-    
     async onInit() {
         // !!!! remove next lines before publishing !!!!
-        
+
         const runningVersion = this.parseVersionString(Homey.manifest.version);
-        if (process.env.DEBUG === '1' || runningVersion.patch % 2 != 0) { // either when running from console or odd patch version
+        if (process.env.DEBUG === '1' || runningVersion.patch % 2 != 0) {
+            // either when running from console or odd patch version
             await LogToFile();
+
             // log at: http://<homey IP>:8008
         }
-        
 
-        this.log(`${Homey.manifest.id} ${Homey.manifest.version}    initialising --------------`);
+        this.log(
+            `${Homey.manifest.id} ${Homey.manifest.version}    initialising --------------`
+        );
 
-        this.log('app.js                     preparing Node environment')
-        if (! global.fetch) {
+        this.log('app.js                     preparing Node environment');
+        if (!global.fetch) {
             global.fetch = require('node-fetch');
         }
-        if (! global.AbortSignal.timeout) {
-            global.AbortSignal.timeout = timeout => {
+        if (!global.AbortSignal.timeout) {
+            global.AbortSignal.timeout = (timeout) => {
                 const controller = new AbortController();
                 const abort = setTimeout(() => {
-                controller.abort();
+                    controller.abort();
                 }, timeout);
                 return controller.signal;
-            }
-        }
-        this.log('app.js                     preparing Node environment done')
-
-        this.log('app.js                     preparing logging environment')
-            // Override the default stderr.write function
-            process.stderr.write = (chunk, encoding, callback) => {
-                // Append the stderr output to the variable
-                capturedStderr += chunk; 
-                
-                // Process error here
-                This.log('Error captured:', capturedStderr)
-
-                // Write to the original stderr
-                originalStderrWrite.call(process.stderr, chunk, encoding, callback); 
             };
-        this.log('app.js                     preparing logging environment done')
+        }
+        this.log('app.js                     preparing Node environment done');
+
+        this.log('app.js                     preparing logging environment');
+        // Override the default stderr.write function
+        process.stderr.write = (chunk, encoding, callback) => {
+            // Append the stderr output to the variable
+            capturedStderr += chunk;
+
+            // Process error here
+            This.log('Error captured:', capturedStderr);
+
+            // Write to the original stderr
+            originalStderrWrite.call(process.stderr, chunk, encoding, callback);
+        };
+        this.log(
+            'app.js                     preparing logging environment done'
+        );
 
         this.lastLocationModes = [];
         this.alarmSystem = {};
-        this.alarmSystem.location = {}
+        this.alarmSystem.location = {};
 
         this._api = new api(this.homey);
 
-        this._api.on('ringOnNotification',this._ringOnNotification.bind(this));
-        this._api.on('ringOnDing',this._ringOnDing.bind(this));
-        this._api.on('ringOnData',this._ringOnData.bind(this));
-        this._api.on('ringOnAlarmData',this._ringOnAlarmData.bind(this));
+        this._api.on('ringOnNotification', this._ringOnNotification.bind(this));
+        this._api.on('ringOnDing', this._ringOnDing.bind(this));
+        this._api.on('ringOnData', this._ringOnData.bind(this));
+        this._api.on('ringOnAlarmData', this._ringOnAlarmData.bind(this));
         this._api.on('ringOnLocation', this._ringOnLocation.bind(this));
 
-        this._triggerLocationModeChangedTo = this.homey.flow.getTriggerCard('ring_location_mode_changed_generic');
+        this._triggerLocationModeChangedTo = this.homey.flow.getTriggerCard(
+            'ring_location_mode_changed_generic'
+        );
         this.registerLocationModeChanged();
 
-        this._conditionLocationMode = this.homey.flow.getConditionCard('ring_location_mode_active');
+        this._conditionLocationMode = this.homey.flow.getConditionCard(
+            'ring_location_mode_active'
+        );
         this.conditionLocationMode();
 
-        this._setLocationMode = this.homey.flow.getActionCard('change_location_mode');
+        this._setLocationMode = this.homey.flow.getActionCard(
+            'change_location_mode'
+        );
         this.setLocationMode();
 
-        this.log(`${Homey.manifest.id} ${Homey.manifest.version}    initialising done ---------`);
+        this.log(
+            `${Homey.manifest.id} ${Homey.manifest.version}    initialising done ---------`
+        );
 
         await this._api.init();
 
-        let logLine = " app.js || onInit || --------- " + `${Homey.manifest.id} ${Homey.manifest.version} started ---------`;
+        let logLine =
+            ' app.js || onInit || --------- ' +
+            `${Homey.manifest.id} ${Homey.manifest.version} started ---------`;
         this.homey.app.writeLog(logLine);
     }
 
@@ -92,7 +105,7 @@ class App extends Homey.App {
 
     // Called from event emitted from _connectRingAPI() in Api.js
     _ringOnDing(device) {
-        this.homey.emit("ringOnDing", device);
+        this.homey.emit('ringOnDing', device);
     }
 
     // Called from event emitted from _connectRingAPI() in Api.js
@@ -102,12 +115,12 @@ class App extends Homey.App {
 
     // Called from event emitted from _connectRingAPI() in Api.js for Ring Alarm devices
     _ringOnAlarmData(data) {
-        if ( data.catalogId == this.alarmSystem.catalogId ) {
-            if ( this.alarmSystem.mode != data.mode ) {
+        if (data.catalogId == this.alarmSystem.catalogId) {
+            if (this.alarmSystem.mode != data.mode) {
                 // Mode changed
                 this.log('this.alarmSystem: ', this.alarmSystem);
-                
-                this.alarmSystem.mode = data.mode
+
+                this.alarmSystem.mode = data.mode;
             }
         }
 
@@ -117,26 +130,30 @@ class App extends Homey.App {
     // Called from event emitted from _connectRingAPI() in Api.js
     _ringOnLocation(newLocationMode) {
         //this.log('_ringOnLocation',newLocationMode);
-        if(this.lastLocationModes.length>0)
-        {
-            let matchedLastLocationMode = this.lastLocationModes.find(lastLocationMode =>{
-                 return lastLocationMode.id==newLocationMode.id;
-            });
-            if(matchedLastLocationMode!=undefined)
-            {
+        if (this.lastLocationModes.length > 0) {
+            let matchedLastLocationMode = this.lastLocationModes.find(
+                (lastLocationMode) => {
+                    return lastLocationMode.id == newLocationMode.id;
+                }
+            );
+            if (matchedLastLocationMode != undefined) {
                 //console.log('Check location mode for remembered location '+matchedLastLocationMode.name+' was in mode '+matchedLastLocationMode.mode+' and now is in mode '+newLocationMode.mode);
-                if(matchedLastLocationMode.mode!=newLocationMode.mode)
-                {
+                if (matchedLastLocationMode.mode != newLocationMode.mode) {
                     //console.log('location mode changed, raise the flow trigger!');
-                    this.triggerLocationModeChanged({oldmode: matchedLastLocationMode.mode, mode: newLocationMode.mode},{location: newLocationMode});
+                    this.triggerLocationModeChanged(
+                        {
+                            oldmode: matchedLastLocationMode.mode,
+                            mode: newLocationMode.mode,
+                        },
+                        { location: newLocationMode }
+                    );
                 }
                 matchedLastLocationMode.mode = newLocationMode.mode;
-            }
-            else {
+            } else {
                 //console.log('recevied new location mode for location '+newLocationMode.name+', there is no old state known for this location');
                 this.lastLocationModes.push(newLocationMode);
             }
-        } else{
+        } else {
             //console.log('recevied new location mode for location '+newLocationMode.name+', there is no old state known for this location');
             this.lastLocationModes.push(newLocationMode);
         }
@@ -195,7 +212,7 @@ class App extends Homey.App {
     }
 
     logRealtime(event, details) {
-        this.homey.api.realtime(event, details)
+        this.homey.api.realtime(event, details);
         // this.log('Realtime event emitted for', event, details);
     }
 
@@ -208,7 +225,9 @@ class App extends Homey.App {
     registerLocationModeChanged() {
         this._triggerLocationModeChangedTo
             .registerRunListener((args, state) => {
-                return Promise.resolve( args.location.name === state.location.name );
+                return Promise.resolve(
+                    args.location.name === state.location.name
+                );
             })
             .getArgument('location')
             .registerAutocompleteListener((query, args) => {
@@ -225,10 +244,12 @@ class App extends Homey.App {
         this._conditionLocationMode
             .registerRunListener((args, state) => {
                 return new Promise((resolve, reject) => {
-                    var matchedLocationMode = this.lastLocationModes.find(lastLocationMode =>{
-                        return lastLocationMode.id==args.location.id;
-                    });
-                    if(matchedLocationMode!=undefined) {
+                    var matchedLocationMode = this.lastLocationModes.find(
+                        (lastLocationMode) => {
+                            return lastLocationMode.id == args.location.id;
+                        }
+                    );
+                    if (matchedLocationMode != undefined) {
                         //this.log ('stored location mode found for location ' + matchedLocationMode.name);
                         resolve(matchedLocationMode.mode === args.mode);
                     } else {
@@ -240,9 +261,9 @@ class App extends Homey.App {
             .getArgument('location')
             .registerAutocompleteListener((query, args) => {
                 return new Promise(async (resolve) => {
-                const locations = await this._api.userLocations();
-                //this.log ('I found these locations',locations);
-                resolve(locations);
+                    const locations = await this._api.userLocations();
+                    //this.log ('I found these locations',locations);
+                    resolve(locations);
                 });
             });
     }
@@ -253,21 +274,22 @@ class App extends Homey.App {
             .registerRunListener(async (args, state) => {
                 //this.log ('attempt to switch location ('+args.location.name+') to new state: '+args.mode);
                 return new Promise((resolve, reject) => {
-                    this._api.setLocationMode(args.location.id,args.mode)
+                    this._api
+                        .setLocationMode(args.location.id, args.mode)
                         .then(() => {
                             resolve(true);
                         })
                         .catch((error) => {
                             reject(error);
-                        })
+                        });
                 });
             })
             .getArgument('location')
             .registerAutocompleteListener((query, args) => {
                 return new Promise(async (resolve) => {
-                const locations = await this._api.userLocations();
-                //this.log ('I found these locations',locations);
-                resolve(locations);
+                    const locations = await this._api.userLocations();
+                    //this.log ('I found these locations',locations);
+                    resolve(locations);
                 });
             });
     }
@@ -276,15 +298,13 @@ class App extends Homey.App {
     async getDevicesInfo() {
         //this.log('getDevicesInfo is called through api.js')
         return new Promise((resolve, reject) => {
-
             this.homey.app.getRingDevices((error, result) => {
                 if (error) {
-                return reject(error);
+                    return reject(error);
                 }
 
                 resolve(result);
             });
-
         });
     }
 
@@ -292,23 +312,27 @@ class App extends Homey.App {
     // - Called from multiple functions
     async writeLog(logLine) {
         let savedHistory = this.homey.settings.get('myLog');
-        if ( savedHistory != undefined ) {
+        if (savedHistory != undefined) {
             // cleanup history
             let lineCount = savedHistory.split(/\r\n|\r|\n/).length;
-            if ( lineCount > 2000 ) {
-                let deleteItems = parseInt( lineCount * 0.2 );
+            if (lineCount > 2000) {
+                let deleteItems = parseInt(lineCount * 0.2);
                 let savedHistoryArray = savedHistory.split(/\r\n|\r|\n/);
-                let cleanUp = savedHistoryArray.splice(-1*deleteItems, deleteItems, "" );
+                let cleanUp = savedHistoryArray.splice(
+                    -1 * deleteItems,
+                    deleteItems,
+                    ''
+                );
                 savedHistory = savedHistoryArray.join('\n');
             }
             // end cleanup
-            logLine = this.getDateTime() + logLine + "\n" + savedHistory;
+            logLine = this.getDateTime() + logLine + '\n' + savedHistory;
         } else {
-            this.log("writeLog: savedHistory is undefined!")
+            this.log('writeLog: savedHistory is undefined!');
         }
-        this.homey.settings.set('myLog', logLine );
+        this.homey.settings.set('myLog', logLine);
 
-        logLine = "";
+        logLine = '';
     }
 
     // Support functions
@@ -316,28 +340,47 @@ class App extends Homey.App {
     // Returns a date timestring including milliseconds to be used in loglines
     // - Called from multiple functions
     getDateTime() {
-        let timezone = this.homey.clock.getTimezone()
-        let date = new Date(new Date().toLocaleString("en-US", {timeZone: timezone}));
+        let timezone = this.homey.clock.getTimezone();
+        let date = new Date(
+            new Date().toLocaleString('en-US', { timeZone: timezone })
+        );
         let dateMsecs = new Date();
 
         let hour = date.getHours();
-        hour = (hour < 10 ? "0" : "") + hour;
-        let min  = date.getMinutes();
-        min = (min < 10 ? "0" : "") + min;
-        let sec  = date.getSeconds();
-        sec = (sec < 10 ? "0" : "") + sec;
-        let msec = ("00" + dateMsecs.getMilliseconds()).slice(-3)
+        hour = (hour < 10 ? '0' : '') + hour;
+        let min = date.getMinutes();
+        min = (min < 10 ? '0' : '') + min;
+        let sec = date.getSeconds();
+        sec = (sec < 10 ? '0' : '') + sec;
+        let msec = ('00' + dateMsecs.getMilliseconds()).slice(-3);
         let year = date.getFullYear();
         let month = date.getMonth() + 1;
-        month = (month < 10 ? "0" : "") + month;
-        let day  = date.getDate();
-        day = (day < 10 ? "0" : "") + day;
-        return day + "-" + month + "-" + year + "  ||  " + hour + ":" + min + ":" + sec + "." + msec + "  ||  ";
+        month = (month < 10 ? '0' : '') + month;
+        let day = date.getDate();
+        day = (day < 10 ? '0' : '') + day;
+        return (
+            day +
+            '-' +
+            month +
+            '-' +
+            year +
+            '  ||  ' +
+            hour +
+            ':' +
+            min +
+            ':' +
+            sec +
+            '.' +
+            msec +
+            '  ||  '
+        );
     }
 
     // returns the supplied version is a usable format; version.major, version.minor, version.path
     parseVersionString(version) {
-        if (typeof(version) != 'string') { return false; }
+        if (typeof version != 'string') {
+            return false;
+        }
         var x = version.split('.');
         // parse from string or default to 0 if can't parse
         var maj = parseInt(x[0]) || 0;
@@ -346,10 +389,9 @@ class App extends Homey.App {
         return {
             major: maj,
             minor: min,
-            patch: pat
-        }
+            patch: pat,
+        };
     }
-
 }
 
 module.exports = App;
